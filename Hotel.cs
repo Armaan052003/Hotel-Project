991d6d119102919b3ac3450e96052d5a0a690f22
Gourav052003 <gourav052003@gmail.com>
2024-06-04 14:56:08 +0530

    Project added

---
+++ Hotel.cs
@@ -0,0 +1,179 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters;
+
+public class HotelRooms
+{   
+    
+
+    // user side details
+    private int _id;
+    private string _username;
+
+    private Dictionary<string, int> _user_rooms_bookingDays = new Dictionary<string, int>();
+    private List<Dictionary<string, List<string>>> _user_rooms_specialities = new List<Dictionary<string, List<string>>>();
+
+
+    // System Details
+    private int _totalBookingPrice = 0;
+
+    private Dictionary<string, int> _FixedBookingPrice = new Dictionary<string, int>
+    {
+        {"Gold",4500}, 
+        {"Silver",3000},
+        {"Bronze",2000}
+
+    };
+
+    
+    private List<Dictionary<string, Dictionary<string, int>>> _rooms_speciality_data = new List<Dictionary<string, Dictionary<string, int>>>
+    {
+        new Dictionary<string, Dictionary<string, int>>
+        {
+            {
+                "Gold", new Dictionary<string, int>
+                {
+                    {"Ac",1300 },
+                    {"Two Beds",2000},
+                    {"One Bed",1500},
+                    {"None",0}
+
+                }
+            }
+        },
+
+        new Dictionary<string, Dictionary<string, int>>
+        {
+            {
+                "Silver", new Dictionary<string, int>
+                {
+                    {"Ac", 1200 },
+                    {"Two Beds",1800},
+                    {"One Bed",1300},
+                    {"None",0}
+
+                }
+            }
+        },
+
+        new Dictionary<string, Dictionary<string, int>>
+        {
+            {
+                "Silver", new Dictionary<string, int>
+                {
+                    {"Ac", 1000 },
+                    {"Two Beds",1500},
+                    {"One Bed",1100},
+                    {"None",0}
+
+                }
+            }
+        }
+    };
+
+    public HotelRooms(int id,string username)
+    {
+        this._id = id;
+        this._username = username;
+    }
+
+  
+    public void provideBookingInformation(List<Dictionary<string, List<string>>> user_room_specialties, Dictionary<string, int> user_rooms_bookingDays)
+    {
+        foreach (Dictionary<string, List<string>> room_speciality in user_room_specialties)
+        {
+            foreach (KeyValuePair<string, List<string>> room_details in room_speciality)
+            {
+                foreach (Dictionary<string, Dictionary<string, int>> room_specialty_data in _rooms_speciality_data)
+                {
+                    if (room_specialty_data.ContainsKey(room_details.Key))
+                    {
+                        foreach (string speciality in room_details.Value)
+                        {
+                            if (room_specialty_data[room_details.Key].ContainsKey(speciality))
+                            {
+                                _user_rooms_specialities.Add(
+
+                                    new Dictionary<string, List<string>>
+                                    {
+                                            {
+                                                room_details.Key, new List<string>(room_details.Value)
+                                            }
+                                    }
+
+                                );
+                            }
+                        }
+
+                        _user_rooms_bookingDays.Add(room_details.Key, user_rooms_bookingDays[room_details.Key]);
+                    }
+                }
+            }
+        }
+
+    }  
+
+    public void getBookingBill()
+    {
+        foreach(Dictionary<string,List<string>> userRoom in _user_rooms_specialities)
+        {
+            foreach(KeyValuePair<string, List<string>> userRoom_Speciality in userRoom)
+            {
+                _totalBookingPrice += _FixedBookingPrice[userRoom_Speciality.Key];
+
+                foreach (string speciality in userRoom_Speciality.Value)
+                {
+                    foreach(Dictionary<string, Dictionary<string, int>> roomData in _rooms_speciality_data)
+                    {
+                        foreach(KeyValuePair<string,Dictionary<string, int>> specialites_data in roomData)
+                        {
+                            _totalBookingPrice+=specialites_data.Value[speciality];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public void displayUserRoomsBookingInformation()
+    {
+
+    }
+    public void displayRoomsInformation()
+    {
+        Console.WriteLine("Room Fixed Charges per day : \n");
+
+        foreach(KeyValuePair<string,int> roomFixedCharges in _FixedBookingPrice)
+        {
+            Console.WriteLine($"{roomFixedCharges.Key} => {roomFixedCharges.Value} INR\n");
+
+            foreach (Dictionary<string, Dictionary<string, int>> roomData in _rooms_speciality_data)
+            {
+                Console.WriteLine("Extra Prices for Specialiteis : ");
+
+                foreach (KeyValuePair<string, Dictionary<string, int>> specialites_data in roomData)
+                {
+                    foreach(KeyValuePair<string,int> SpecialtiyPrice in specialites_data.Value)
+                    {
+                        Console.WriteLine($"{SpecialtiyPrice.Key} = {SpecialtiyPrice.Value} INR");
+                    }
+                    
+                }
+            }
+
+        }
+
+        
+    }
+                                              
+
+    public static void Main(string[] args)
+    {
+
+
+    
+    }
+}
---
+++ MyHotel.cs
@@ -0,0 +1,308 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Hotel
+{   
+    
+    private static int _bookingId = 0;
+    private Dictionary<int, Hotel> database = new Dictionary<int, Hotel>();
+
+    // user side details
+    
+    private int _id;
+    private string _username;
+
+    private Dictionary<string, int> _user_rooms_bookingDays = new Dictionary<string, int>();
+    private Dictionary<string, List<string>> _user_rooms_specialities = new Dictionary<string, List<string>>();
+
+    private Dictionary<string, int> _user_rooms_booked = new Dictionary<string, int>
+    {
+
+        { "Gold",0},
+        {"Silver",0 },
+        {"Bronze",0 }
+
+    };
+
+
+    // System Details
+    private int _totalBookingPrice = 0;
+
+
+    private static Dictionary<string, int> _rooms_availiable = new Dictionary<string, int> {
+
+        { "Gold",5},
+        {"Silver",10 },
+        {"Bronze",15 }
+
+    };
+
+    private Dictionary<string, int> _FixedBookingPrice = new Dictionary<string, int>
+    {
+        {"Gold",4500}, 
+        {"Silver",3000},
+        {"Bronze",2000}
+
+    };
+
+    
+    private Dictionary<string, Dictionary<string, int>> _rooms_speciality_data = new Dictionary<string, Dictionary<string, int>>
+    {
+       
+        {
+            "Gold", new Dictionary<string, int>
+            {
+                {"Ac",1300 },
+                {"Two Beds",2000},
+                {"One Bed",1500},
+                {"None",0}
+
+            }
+        },
+        
+
+        
+        {
+            "Silver", new Dictionary<string, int>
+            {
+                {"Ac", 1200 },
+                {"Two Beds",1800},
+                {"One Bed",1300},
+                {"None",0}
+
+            }
+        },
+       
+
+        
+        {
+            "Bronze", new Dictionary<string, int>
+            {
+                {"Ac", 1000 },
+                {"Two Beds",1500},
+                {"One Bed",1100},
+                {"None",0}
+
+            }
+        },
+        
+    };
+
+    public Hotel(int id,string username)
+    {
+        _id = id;
+        _username = username;
+    }
+
+  
+    public void getBookingBill()
+    {
+        _totalBookingPrice = 0;
+        int price = 0;
+
+        foreach(KeyValuePair<string,List<string>> userRoom in _user_rooms_specialities)
+        {
+            
+            price += _FixedBookingPrice[userRoom.Key];
+
+            foreach (string speciality in userRoom.Value)
+            {
+
+                price += _rooms_speciality_data[userRoom.Key][speciality];
+                           
+            }
+
+            price *= _user_rooms_booked[userRoom.Key] * _user_rooms_bookingDays[userRoom.Key];
+            _totalBookingPrice+= price;
+            price = 0;
+        }
+
+        
+        Console.WriteLine($"\nYour Booking Bill is {_totalBookingPrice} INR/- Only");
+        database.Add(_bookingId, this);
+    }
+    
+    public void getMyBookingDetails()
+    {
+        foreach(KeyValuePair<string,List<string>> user_room_speciality in _user_rooms_specialities)
+        {
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine($"Room Type - {user_room_speciality.Key}, No. of Rooms = {_user_rooms_booked[user_room_speciality.Key]} No. of Days = {_user_rooms_bookingDays[user_room_speciality.Key]}");
+
+            foreach(string speciality in user_room_speciality.Value)
+            {
+                int cost = _rooms_speciality_data[user_room_speciality.Key][speciality];
+                Console.WriteLine($"{speciality} = {cost} INR");
+            }
+        }
+    }
+ 
+    public void displayRoomsInformation()
+    {
+        Console.WriteLine("\n********************************************************");
+
+        foreach (KeyValuePair<string,int> roomFixedCharges in _FixedBookingPrice)
+        {
+                Console.WriteLine($"\nRoom Fixed Charges per day : {roomFixedCharges.Key} Room => {roomFixedCharges.Value} INR\n");
+
+               
+                Console.WriteLine("Extra Prices for Specialities : \n");
+
+                foreach (KeyValuePair<string, int> specialites_data in _rooms_speciality_data[roomFixedCharges.Key])
+                {
+                    Console.WriteLine($"{specialites_data.Key} = {specialites_data.Value} INR");   
+                }
+
+            Console.WriteLine("\n********************************************************");
+        }
+
+        
+    }
+                 
+    public void takeBooking()
+    {   
+
+        List<string> specialities = new List<string>();
+        int nRooms=0, nDays=0;
+
+        Console.WriteLine("\n================================================================");
+        Console.WriteLine($"1. Gold");
+        Console.WriteLine($"2. Silver");
+        Console.WriteLine($"3. Bronze\n");
+
+        Console.Write("Choose Room (by Typing only name as Shown in List) : ");
+        string roomType = Console.ReadLine();
+        Console.WriteLine();
+
+        if (_rooms_speciality_data.ContainsKey(roomType))
+        {
+            foreach (KeyValuePair<string, int> specialites_data in _rooms_speciality_data[roomType])
+            {
+                Console.WriteLine($"{specialites_data.Key} = {specialites_data.Value} INR");
+            }
+
+            Console.Write("\nChoose Speciality (by Typing only name as Shown in List) : ");
+           
+            Console.WriteLine("Enter NONE to exit..");
+
+            while (true)
+            {
+                Console.Write("Speciality Name = ");
+                string specialityName = Console.ReadLine();
+
+                if (specialityName != "NONE")
+                {
+                    if (_rooms_speciality_data[roomType].ContainsKey(specialityName))
+                    {
+                        specialities.Add(specialityName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{specialityName} not availiable");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.Write("No. of Rooms = ");
+            int temp_nRooms = int.Parse(Console.ReadLine());
+
+            
+            if (_rooms_availiable[roomType] == 0)
+            {
+                Console.WriteLine("Rooms Not availiable for this category.. ");
+                return;
+            }
+
+            else if ((_rooms_availiable[roomType] - temp_nRooms) > 0)
+            {
+
+                if (temp_nRooms < 0) 
+                {
+                    Console.WriteLine("Please enter greater than or equal to zero number of Rooms.. ");
+                    
+                    while(temp_nRooms<0)
+                    {
+                        Console.Write("No. of Rooms = ");
+                        temp_nRooms = int.Parse(Console.ReadLine());
+                    }
+                    
+
+                }
+
+
+                nRooms = temp_nRooms;
+
+            }
+
+            if ((_rooms_availiable[roomType] - temp_nRooms) < 0)
+            {   
+                Console.WriteLine($" Only {_rooms_availiable[roomType]} availiable for this category ..");
+                
+                while(((_rooms_availiable[roomType] - temp_nRooms) <0) || (temp_nRooms < 0))
+                {
+                    Console.Write("No. of Rooms (greater than eaual or to zero) = ");
+                    temp_nRooms = int.Parse(Console.ReadLine());
+                }
+
+                nRooms = temp_nRooms;
+            }
+
+            Console.Write("No of Booking Days = ");
+            int temp_nDays = int.Parse(Console.ReadLine());
+
+            if (temp_nDays > 0) 
+            {
+                nDays = temp_nDays;
+            }
+            else
+            {
+                Console.WriteLine("Please Enter No. of Booking days greater than 0");
+                while(temp_nDays<=0)
+                {
+                    Console.Write("No of Booking Days = ");
+                    temp_nDays = int.Parse(Console.ReadLine());
+                }
+
+                nDays = temp_nDays;
+            }
+            
+
+
+        }
+        else
+        {
+            Console.WriteLine("Booking Failed..");
+            return;
+        }
+
+        _user_rooms_specialities.Add(roomType, specialities);
+        _user_rooms_bookingDays.Add(roomType, nDays);
+ 
+        _user_rooms_booked[roomType] += nRooms;
+        _rooms_availiable[roomType] -= nRooms;
+
+        _bookingId++;
+    }
+    
+
+    public static void Main(string[] args)
+    {
+        Hotel customer1 = new Hotel(1001, "Mohan Kumar");
+
+        customer1.displayRoomsInformation();
+        customer1.takeBooking();
+        customer1.getMyBookingDetails();
+        customer1.getBookingBill();
+
+        customer1.displayRoomsInformation();
+        customer1.takeBooking();
+        customer1.getMyBookingDetails();
+        customer1.getBookingBill();
+
+    }
+}
